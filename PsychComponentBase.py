e1fbd1e2c70c07ee9405af5126dd274509bb3945
Arthur <lugtigheid@gmail.com>
2013-08-03 17:27:59 -0400

    Added top level class

---
+++ PsychComponentBase.py
@@ -0,0 +1,42 @@
+import random;
+
+class PsychComponentBase:
+
+	def __init__(self, NumAFC = 1, GiveFeedback = False, Conditions = None, OutputFile = ''):
+		random.seed();
+
+		# These are all the parameters that are common to all procedures
+		self._NumAFC = NumAFC;
+		self._GiveFeedback = GiveFeedback;
+		self._Conditions = Conditions;
+		self._ChoiceAnswers;
+		self._Status; 
+		self._ActiveTrialIndex;
+		self._OutputFile = OutputFile;
+
+	def GetRandomInterval(self):
+		return random.randint(1,self.NumberAFC)
+
+	def GetTrial(self, index):
+		pass
+
+	def Initialise():
+		pass
+	
+	def InvokeFeedbackEvent(): 
+		pass 
+
+	#all logging should also be in here
+
+	def GetConditionsDetails(): pass
+	def GetExperimentHeader(): pass
+	def GetParameterDetails(): pass
+	def GetTrialHeader(): pass
+	def GetResponseInfo(): pass
+
+
+
+Psy = PsychComponentBase();
+print Psy.GetRandomInterval();
+
+	
