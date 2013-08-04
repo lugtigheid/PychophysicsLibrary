37b0aed11a5b965bfeab120703e8a13ccd7133a8
ghcmat <cutonem@yorku.ca>
2013-08-04 19:35:04 -0400

    fixing indentation

--- ConstantStimulusPsychComponent.py
+++ ConstantStimulusPsychComponent.py
@@ -2,40 +2,40 @@ from Trial import *
 
 class ConstantStimulusPsychComponent(object):
 
-	def __init__(self, *args, **kwargs):
-		''' Constructor: sets up the variables '''
+    def __init__(self, *args, **kwargs):
+        ''' Constructor: sets up the variables '''
 
-		self._StimulusValues = list();
-		self._Conditions = None;
+        self._StimulusValues = list();
+        self._Conditions = None;
 
 
-	def Initialise(self):
-		pass
+    def Initialise(self):
+        pass
 
-	def GetNextTrial(self):
-		trial = Trial();
-		return trial;
+    def GetNextTrial(self):
+        trial = Trial();
+        return trial;
 
-	def AddStimval(self, stimval):
-		
-		# if this is a number (float, long, int, complex) add it. 
-		if isinstance(stimval, (int, long, float)) : 
-			self._StimulusValues.append(stimval)
+    def AddStimval(self, stimval):
+        
+        # if this is a number (float, long, int, complex) add it. 
+        if isinstance(stimval, (int, long, float)) : 
+            self._StimulusValues.append(stimval)
 
-		# otherwise if this is a list we add all items to the list
-		elif type(stimval) is list:
+        # otherwise if this is a list we add all items to the list
+        elif type(stimval) is list:
 
-			# loop through the list
-			for item in stimval:
+            # loop through the list
+            for item in stimval:
 
-				'''but check if numeric before adding (otherwise 
-					I would have just used .extend() here) '''
-				if isinstance(item, (int, long, float)): 
-					self._StimulusValues.append(item)
+                '''but check if numeric before adding (otherwise 
+                    I would have just used .extend() here) '''
+                if isinstance(item, (int, long, float)): 
+                    self._StimulusValues.append(item)
 
-		else:
-			print "ERROR: can only add numbers of lists of numbers to stimulus values!"
+        else:
+            print "ERROR: can only add numbers of lists of numbers to stimulus values!"
 
-	def Evaluate(self, trial):
-		pass
+    def Evaluate(self, trial):
+        pass
 
