2ef4a32bb8aae582180a7915d54114c572cd507a
ghcmat <cutonem@yorku.ca>
2013-08-04 01:29:27 -0400

    fixing indentation in Trail.py

--- Trial.py
+++ Trial.py
@@ -1,22 +1,22 @@
 class Trial(object):
 
-	''' 
-		This class contains all variables associated with a single trial 
+    ''' 
+        This class contains all variables associated with a single trial 
 
-		Fields:
-			
-			TrialID
-			StaircaseID (specific to staircases)
-			Condition
-			Stimulus value
-			Interval
-			Response
-			Reaction time
+        Fields:
+            
+            TrialID
+            StaircaseID (specific to staircases)
+            Condition
+            Stimulus value
+            Interval
+            Response
+            Reaction time
 
-	'''
+    '''
 
-	def __init__(self):
-		print 'initialising this biatch'
+    def __init__(self):
+        print 'initialising this biatch'
 
     def GetTrialData(self):
         ''' Return a list of data from this trial '''
